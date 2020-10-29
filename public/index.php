c02e8e36e87dd6ea0f5590aa322af104432a6476
林駿綦 <r10101720307@gmail.com>
2020-10-29 12:22:49 +0800

    測試 Monolog ，並在 .gitignore 忽略 /log

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require '../vendor/autoload.php';
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 $world = new World();
 $lara = new Lara();
@@ -12,3 +14,11 @@ $vincent= new Hello\Someone('Vincent');
 // 以下Someone類別的使用可以不用use Demo\Hello;
 $mary= new \Demo\Hello\Someone('Mary');
 $john= new Demo\Hello\Someone('John');
+
+// create a log channel
+$log = new Logger('laravel');
+$log->pushHandler(new StreamHandler('../log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
